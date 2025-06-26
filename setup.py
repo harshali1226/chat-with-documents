972a804ce8cd52ed0a21eeb41e4530638d71ed23
Harshali Gaikwad <hgaikwad@hawk.iit.edu>
2025-06-26 15:20:47 -0500

    New files added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Harshali',
+    author_email='harshali1226@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
